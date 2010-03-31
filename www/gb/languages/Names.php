8f870fd4d1c36448657019978203a8ebbd1e071f
Jeff Case <jeffrey.p.case@gmail.com>
2010-03-31 19:32:08 +0900

    Initial commit
    * Code dump of MediaWiki after initial setup was completed
    * from MieJETs.org server

---
+++ www/gb/languages/Names.php
@@ -0,0 +1,11 @@
+<?php
+/**
+  * These determine things like interwikis, language selectors, and so on.
+  * Safe to change without running scripts on the respective sites.
+  *
+  * @ingroup Language
+  */
+/* private */ $wgLanguageNames = array(
+	'en' => 'English',		# English
+	'en-gb' => 'British English',		# British English
+);
